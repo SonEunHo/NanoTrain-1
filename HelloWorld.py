efd062b7d161b009737037252f715f03e67f3d84
nano.son <nano.son@kakaocorp.com>
2018-05-07 20:34:24 +0900

    request 정보 분석중(파라미터 일부 분석)

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,4 +3,81 @@ import math
 print ("hello")
 print (str(math.sqrt(20)))
 
+temp = """
+
+검색 -> 예매하기 -> 좌석 고르기 -> 결
+
+cookie : JSESSESIONID, PCID, RC_COLOR, RC_RESOLUTION
+
+지역 코드
+몇시차, 인원, 좌석위치
+
+var srt = ['0514', '0036', '0507', '0037', '0010', '0015', '0552', '0041', '0020', '0551', '0508', '0297', '0509', '0030', '0033', '0553', '0502'];
+<ul>
+						<li><a href="#none" onclick="selectStnCd(this, '0551', '수서'); return false;" class="map-01">수서</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0552', '동탄'); return false;" class="map-02">동탄</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0553', '지제'); return false;" class="map-03">지제</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0502', '천안아산'); return false;" class="map-04">천안아산</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0297', '오송'); return false;" class="map-05">오송</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0010', '대전'); return false;" class="map-06">대전</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0507', '김천구미'); return false;" class="map-07">김천구미</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0015', '동대구'); return false;" class="map-08">동대구</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0508', '신경주'); return false;" class="map-09">신경주</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0509', '울산'); return false;" class="map-10">울산</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0020', '부산'); return false;" class="map-11">부산</a></li>
+					</ul>
+				</li>
+				<li>호남고속선
+					<ul>
+						<li>수서</li>
+						<li>동탄</li>
+						<li>지제</li>
+						<li>천안아산</li>
+						<li>오송</li>
+						<li><a href="#none" onclick="selectStnCd(this, '0514', '공주'); return false;" class="map-12">공주</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0030', '익산'); return false;" class="map-14">익산</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0033', '정읍'); return false;" class="map-15">정읍</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0036', '광주송정'); return false;" class="map-16">광주송정</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0037', '나주'); return false;" class="map-17">나주</a></li>
+						<li><a href="#none" onclick="selectStnCd(this, '0041', '목포'); return false;" class="map-18">목포</a></li>
+
+
+도착역 코드    arvRsStnCd	0015
+도착역 이름    arvRsStnCdNm	동대구
+???    chtnDvCd	1
+출발날짜    dptDt	20180507
+출발역 코드    dptRsStnCd	0551
+출발역 이    dptRsStnCdNm	수서
+출발시간    dptTm	150500
+리퀘스트인지..    isRequest	Y (불변)
+사람수(어른)    psgInfoPerPrnb1	1
+사람수(장애 1~3급)    psgInfoPerPrnb2	0
+사람수(장애 4~6급)    psgInfoPerPrnb3	0
+사람수(만65세 이상)    psgInfoPerPrnb4	0
+사람수(만4세~12세)   psgInfoPerPrnb5	0
+사람수(종합)    psgNum	1  (종합 수는 9명 이하여야한다.)
+좌석속성    rqSeatAttCd1 015  (일반015 / 휠체어 021 / 전동휠체어 028)
+좌석위    seatAttCd	015 (암때나 015 / 1인석 011/ 창측좌석 012 / 내측좌석 013) 
+???    stlbTrnClsfCd	05
+차종구분    trnGpCd	109 (전체 109 / SRT 300 / SRT+KTX 900)
+
+승차권종류 : pageId	TK0101010000 (일반승차권 TK0101010000 / 단체승차권 TK0101020000)
+
+
+    <select name="locSeatAttCd1" class="prnb checkForm" option="{isMust : false, message : '좌석위치를 선택하십시오.'}" title="좌석위치 선택">
+        <option value="000" selected="selected">좌석위치</option>
+        <option value="011">1인석</option>
+        <option value="012">창측좌석</option>
+        <option value="013">내측좌석</option>
+    </select>
+    <select name="rqSeatAttCd1" class="prnb checkForm" option="{isMust : false, message : '좌석속성을 선택하십시오.'}" title="좌석속성 선택">
+        <option value="015">좌석속성</option>
+        <option value="015" selected="selected">일반</option>
+        <option value="021">휠체어</option>
+        <option value="028">전동휠체어</option>
+    </select>
+    
+    
+
+"""
 
